c87f8fbc602ef8238d0c1e8f7f55fd9572c10bf4
jacksonreaction <contactjacksonhill@gmail.com>
2022-07-12 19:53:38 +1000

    Add functions to create container div and grid divs

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-let container = document.getElementsByClassName("container");
+// Determines the amount of squares per side of the grid
 
-for (let i = 0; i < 15; i++) {
-    let divGrid = document.createElement('div')
-    divGrid.style.width = '20px'
-    divGrid.style.height = '20px'
-    divGrid.style.backgroundColor = 'black'
-    divGrid.setAttribute('id', 'div-grid')
-    container[0].appendChild(divGrid)
+let userChosenWidth = 10;
+
+// Selects the grid div and styles its height, width, and display
+
+function styleContainer(className) {
+    let container = document.getElementsByClassName(className);
+    container = container[0];
+    container.style.width = "500px";
+    container.style.height = "100%";
+    container.style.display = "inline-block";
+    return container
 }
+
+function createGridDivs(squaresPerSide, containerName) {
+    totalSquares = Math.pow(squaresPerSide)
+    for (let i = 0; i < totalSquares; i++) {
+        let divGrid = document.createElement('div')
+        divGrid.style.width = '20px'
+        divGrid.style.height = '20px'
+        divGrid.style.backgroundColor = 'black'
+        divGrid.setAttribute('class', 'div-grid')
+        containerName.appendChild(divGrid)
+    }
+}
+
+container = styleContainer("container")
+
+createGridDivs(userChosenWidth, container)
