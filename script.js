44c6013cea074bbd2b9b9625eb3b821f288c1343
jacksonreaction <contactjacksonhill@gmail.com>
2022-07-17 13:38:34 +1000

    Give reset functionality to button, give ability to choose size of grid to button

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-// Determines the amount of squares per side of the grid
-
-let userChosenWidth = 10;
-
 // Selects the grid div and styles its height, width, and display
 
 function selectFirstInClass(className) {
     let element = document.getElementsByClassName(className);
     element = element[0];
     return element
-}
-
-function styleContainer(containerName) {
-    // containerName.style.grid
 }
 
 function mouseEnterEffect(cell) {
@@ -22,27 +14,51 @@ function mouseLeaveEffect(cell) {
     cell.classList.add('div-grid');
 }
 
-function createGridDivs(squaresPerSide) {
+function createResetButton() {
+    button = document.getElementById("reset-btn")
+    let i = 0;
+    let squaresPerSide = 0
 
     container = selectFirstInClass("container")
     styleContainer(container)
 
-    totalSquares = Math.pow(squaresPerSide, 2)
-
-    for (let column = 0; column < squaresPerSide; column++) {
-        for (let row = 0; row < squaresPerSide; row++) {
-            const cell = document.createElement('div');
-            cell.classList.add('div-grid')
-            cell.style.gridColumnStart = column + 1;
-            cell.style.gridColumnEnd = column + 1;
-            cell.style.gridRowStart = row + 1;
-            cell.style.gridRowEnd = row + 1;
-            cell.addEventListener('mouseenter', (e) => mouseEnterEffect(cell))
-            cell.addEventListener('mouseleave', (e) => mouseLeaveEffect(cell))
-            container.appendChild(cell)
+    button.addEventListener("click", () => {
+        while (i < 2) {
+            squaresPerSide = prompt("Please enter the amount of squares each grid edge should have.")
+            squaresPerSide = Number(squaresPerSide)
+            if (squaresPerSide >= 101) {
+                alert("Please select a number lower than 100!")
+            } else {
+                break
+            }
         }
 
-    }
+        gridCells = document.querySelectorAll('.div-grid');
+        gridCells.forEach(cell => {
+            cell.remove();
+        })
+
+        const gridDivs = document.querySelectorAll(".mouse-over")
+        const div = gridDivs[0];
+        gridDivs.forEach(function(div) {
+            div.classList.remove('mouse-over')
+            div.classList.add('div-grid')
+        })
+
+        for (let column = 0; column < squaresPerSide; column++) {
+            for (let row = 0; row < squaresPerSide; row++) {
+                const cell = document.createElement('div');
+                cell.classList.add('div-grid')
+                cell.style.gridColumnStart = column + 1;
+                cell.style.gridColumnEnd = column + 1;
+                cell.style.gridRowStart = row + 1;
+                cell.style.gridRowEnd = row + 1;
+                cell.addEventListener('mouseenter', (e) => mouseEnterEffect(cell))
+                cell.addEventListener('mouseleave', (e) => mouseLeaveEffect(cell))
+                container.appendChild(cell)
+            }
+        }
+    })
 }
 
-createGridDivs(userChosenWidth)
+createResetButton()
